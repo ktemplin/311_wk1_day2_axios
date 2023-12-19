816db67d78e0c067d172638232a9cfa768fcf90b
Kris Templin <kris.templin@gmail.com>
2023-12-18 20:08:11 -0800

    two tests down, one to go

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
+const { default: Axios } = require("axios")
+
 // import axios here
+const axios = require("axios")
 
 // Please note that it is normally not considered best practice to commit 
 // api keys to github as it presents a security risk. It is done here only 
 // for practice purposes as we are sharing the same account
 const api_key = 'd771b19ef336ed8381def3a60b574464'
+const options = {
+  method: 'GET',
+  params: {language: 'en-US'},
+  headers: {accept: 'application/json'},
+  Authorization: 'Bearer d771b19ef336ed8381def3a60b574464'
+};
 
 const discoverMovie = () => {
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`
   // code here
+  return axios.get(url).then(response => {
+    return response
+  })
 }
 
-const getMovieById = (id) => {
-  const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}`
+const getMovieById = async (movie_id) => {
+  const url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${api_key}`
   // code here
+  try {
+    const response = await axios.get(url);
+    return response.data; 
+  } catch (error) {
+    throw error;
+  }
 }
+// getMovieById()
 
 const getMovieByIdFailure = () => {
   const fakeId = 1 // FAKE ID HERE
   const url = `https://api.themoviedb.org/3/movie/${fakeId}?api_key=${api_key}`
   // code here
+
 }
 
 
